47cde7c98f614e0ef7f955a917f81f8cdf0553d6
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2022-08-07 22:44:09 +0200

    circulating supply as plain text output for cmc

--- endpoints/get_circulating_supply.py
+++ endpoints/get_circulating_supply.py
@@ -3,6 +3,7 @@
 from pydantic import BaseModel
 
 from server import app, kaspad_client
+from fastapi.responses import PlainTextResponse
 
 
 class CoinSupplyResponse(BaseModel):
@@ -20,3 +21,12 @@ async def get_coinsupply():
         "circulatingSupply": resp["getCoinSupplyResponse"]["circulatingSompi"],
         "maxSupply": resp["getCoinSupplyResponse"]["maxSompi"]
     }
+
+@app.get("/info/coinsupply/circulating", tags=["Kaspa network info"],
+         response_class=PlainTextResponse)
+async def get_circulating_coins():
+    """
+    Get circulating amount of $KAS token as numerical value
+    """
+    resp = await kaspad_client.request("getCoinSupplyRequest")
+    return str(float(resp["getCoinSupplyResponse"]["circulatingSompi"]) / 100000000)
