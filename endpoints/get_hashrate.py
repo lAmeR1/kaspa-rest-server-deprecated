6b70a6f66a60beb1b8f9f01270ce3aee92eeacd0
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2022-08-25 22:29:37 +0200

    implemented hashrate calculation

--- endpoints/get_hashrate.py
+++ endpoints/get_hashrate.py
@@ -2,7 +2,7 @@
 
 from pydantic import BaseModel
 
-from server import app
+from server import app, kaspad_client
 
 
 class HashrateResponse(BaseModel):
@@ -12,12 +12,34 @@ class HashrateResponse(BaseModel):
 @app.get("/info/hashrate", response_model=HashrateResponse | str, tags=["Kaspa network info"])
 async def get_hashrate(stringOnly: bool = False):
     """
-    DUMMY! Returns the current hashrate for Kaspa network in TH/s.
+    Returns the current hashrate for Kaspa network in TH/s.
     """
+
+    resp = await kaspad_client.request("getBlockDagInfoRequest")
+    hashrate = resp["getBlockDagInfoResponse"]["difficulty"] * 2
+    hashrate_in_th = hashrate / 1_000_000_000_000
+
     if not stringOnly:
         return {
-            "hashrate": 60
+            "hashrate": hashrate_in_th
         }
 
     else:
-        return "60"
+        return f"{hashrate_in_th:.0f}"
+
+
+def __normalize_hashrate(hashrate: int):
+    if hashrate < 1_000:  #
+        return f'{round(hashrate, 2)} H/s'  # Hash
+    elif hashrate < 1_000_000:
+        return f'{round(hashrate / 1_000, 2)} KH/s'  # Kilo
+    elif hashrate < 1_000_000_000:
+        return f'{round(hashrate / 1_000_000, 2)} MH/s'  # Mega
+    elif hashrate < 1_000_000_000_000:
+        return f'{round(hashrate / 1_000_000_000, 2)} GH/s'  # Giga
+    elif hashrate < 1_000_000_000_000_000:
+        return f'{round(hashrate / 1_000_000_000_000, 2)} TH/s'  # Tera
+    elif hashrate < 1_000_000_000_000_000_000:
+        return f'{round(hashrate / 1_000_000_000_000_000, 2)} PH/s'  # Peta
+    elif hashrate < 1_000_000_000_000_000_000_000:
+        return f'{round(hashrate / 1_000_000_000_000_000_000, 2)} EH/s'  # Exa
