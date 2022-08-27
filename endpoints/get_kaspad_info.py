67ec7884b78ca26fb1e97b407d458f5cb28de77c
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2022-08-27 13:27:04 +0200

    p2pid hashed in answer.

--- endpoints/get_kaspad_info.py
+++ endpoints/get_kaspad_info.py
@@ -1,4 +1,5 @@
 # encoding: utf-8
+import hashlib
 
 from pydantic import BaseModel
 
@@ -10,6 +11,7 @@ class KaspadInfoResponse(BaseModel):
     serverVersion: str = "0.12.2"
     isUtxoIndexed: bool = True
     isSynced: bool = True
+    p2pIdHashed : str = "36a17cd8644eef34fc7fe4719655e06dbdf117008900c46975e66c35acd09b01"
 
 
 @app.get("/info/kaspad", response_model=KaspadInfoResponse, tags=["Kaspa network info"])
@@ -18,5 +20,6 @@ async def get_kaspad_info():
     Get some information for kaspad instance, which is currently connected.
     """
     resp = await kaspad_client.request("getInfoRequest")
-    resp["getInfoResponse"].pop("p2pId")
+    p2p_id = resp["getInfoResponse"].pop("p2pId")
+    resp["getInfoResponse"]["p2pIdHashed"] = hashlib.sha256(p2p_id.encode()).hexdigest()
     return resp["getInfoResponse"]
