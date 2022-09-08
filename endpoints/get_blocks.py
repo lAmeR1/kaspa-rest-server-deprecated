a8d30533671ea5c8d28f6db421768e00712721e2
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2022-09-08 02:08:32 +0200

    mergeSetRedsHashes and isChainBlock added to model

--- endpoints/get_blocks.py
+++ endpoints/get_blocks.py
@@ -8,7 +8,6 @@ from pydantic import BaseModel
 from server import app, kaspad_client
 
 
-
 class VerboseDataModel(BaseModel):
     hash: str = "18c7afdf8f447ca06adb8b4946dc45f5feb1188c7d177da6094dfbc760eca699"
     difficulty: float = 4102204523252.94,
@@ -19,6 +18,8 @@ class VerboseDataModel(BaseModel):
                                  "9a822351cd293a653f6721afec1646bd1690da7124b5fbe87001711406010604"
                                  ]
     mergeSetBluesHashes: List[str] = ["580f65c8da9d436480817f6bd7c13eecd9223b37f0d34ae42fb17e1e9fda397e"]
+    mergeSetRedsHashes: List[str] = ["580f65c8da9d436480817f6bd7c13eecd9223b37f0d34ae42fb17e1e9fda397e"]
+    isChainBlock: bool = False
 
 
 class ParentHashModel(BaseModel):
@@ -45,6 +46,7 @@ class BlockModel(BaseModel):
     transactions: list | None
     verboseData: VerboseDataModel
 
+
 class BlockResponse(BaseModel):
     blockHashes: List[str] = ["44edf9bfd32aa154bfad64485882f184372b64bd60565ba121b42fc3cb1238f3",
                               "18c7afdf8f447ca06adb8b4946dc45f5feb1188c7d177da6094dfbc760eca699",
@@ -59,12 +61,13 @@ async def get_block(blockId: str = Path(regex="[a-f0-9]{64}")):
     Get block information for a given block id
     """
     resp = await kaspad_client.request("getBlockRequest",
-                                 params={
-                                     "hash": blockId,
-                                     "includeTransactions": True
-                                 })
+                                       params={
+                                           "hash": blockId,
+                                           "includeTransactions": True
+                                       })
 
     return resp["getBlockResponse"]["block"]
+
 
 @app.get("/blocks", response_model=BlockResponse, tags=["Kaspa blocks"])
 async def get_blocks(lowHash: str = Query(regex="[a-f0-9]{64}"),
@@ -74,10 +77,10 @@ async def get_blocks(lowHash: str = Query(regex="[a-f0-9]{64}"),
     Lists block beginning from a low hash (block id)
     """
     resp = await kaspad_client.request("getBlocksRequest",
-                                 params={
-                                     "lowHash": lowHash,
-                                     "includeBlocks": includeBlocks ,
-                                     "includeTransactions": includeTransactions
-                                 })
+                                       params={
+                                           "lowHash": lowHash,
+                                           "includeBlocks": includeBlocks,
+                                           "includeTransactions": includeTransactions
+                                       })
 
     return resp["getBlocksResponse"]
