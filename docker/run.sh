6992f20968428af99ab7de927b2d6fd82aea41c0
Marius Thøring <mthoring@gmail.com>
2022-11-27 12:55:26 +0100

    Moved docker-stuff to docker/

---
+++ docker/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run --rm supertypo/kaspa-rest-server:latest
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker run --rm -it -p 8000:8000 -e KASPAD_HOST_1=192.168.168.3:16110 supertypo/kaspa-rest-server:latest
-
