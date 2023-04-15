6e8a1d6ef41b40aa2a2b4b4fa6d46276f15c1eaa
lAmeR <42315864+lAmeR1@users.noreply.github.com>
2023-04-15 22:28:53 +0200

    removed block_time from database - not needed

--- models/Transaction.py
+++ models/Transaction.py
@@ -7,8 +7,7 @@ from dbsession import Base
 class Transaction(Base):
     __tablename__ = 'transactions'
     subnetwork_id = Column(String)  # "0000000000000000000000000000000000000000",
-    transaction_id = Column(String,
-                            primary_key=True)  # "bedea078f74f241e7d755a98c9e39fda1dc56491dc7718485a8f221f73f03061",
+    transaction_id = Column(String, primary_key=True)  # "bedea078...1f73f03061",
     hash = Column(String)  # "a5f99f4dc55693124e7c6b75dc3e56b60db381a74716046dbdcae9210ce1052f",
     mass = Column(String)  # "2036",
     block_hash = Column(ARRAY(String))  # "1b41af8cfe1851243bedf596b7299c039b86b2fef8eb4204b04f954da5d2ab0f",
@@ -27,7 +26,6 @@ class TransactionOutput(Base):
     script_public_key_address = Column(String)
     script_public_key_type = Column(String)
     accepting_block_hash = Column(String)
-    block_time = Column(BigInteger)  # "1663286480803"
 
 
 class TransactionInput(Base):
@@ -41,4 +39,3 @@ class TransactionInput(Base):
 
     signature_script = Column(String)  # "41c903159094....281a1d26f70b0037d600554e01",
     sig_op_count = Column(Integer)
-    block_time = Column(BigInteger)  # "1663286480803"
