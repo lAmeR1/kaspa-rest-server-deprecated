e852576f546f32c19fce6831f8f15d488e8a5cb4
lAmeR1 <lamer1@github.com>
2024-05-01 11:03:33 +0200

    invalid escape fix

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import os
 
 IS_TESTNET = os.getenv('TESTNET', 'false').lower() == 'true'
-REGEX_KASPA_ADDRESS = "^kaspa\:[a-z0-9]{61,63}$" if not IS_TESTNET else "^kaspatest\:[a-z0-9]{61,63}$"
+REGEX_KASPA_ADDRESS = "^kaspa:[a-z0-9]{61,63}$" if not IS_TESTNET else "^kaspatest:[a-z0-9]{61,63}$"
 
 # address constants
 ADDRESS_PREFIX = "kaspatest" if IS_TESTNET else "kaspa"
