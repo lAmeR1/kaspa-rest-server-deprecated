6f5706f4f7fae37e1f792747bb0ecc98d9db5267
supertypo <suprtypo@pm.me>
2024-06-20 10:31:48 +0200

    Support testnet address prefix when env var TESTNET is set

--- models/AddressColumn.py
+++ models/AddressColumn.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import TypeDecorator
 from sqlalchemy.types import VARCHAR
 
@@ -6,14 +8,16 @@ class AddressColumn(TypeDecorator):
     impl = VARCHAR
     cache_ok = True
 
-    PREFIX = "kaspa:"
+    prefix = "kaspa:"
+    if os.getenv('TESTNET') == 'true':
+        prefix = "kaspatest:"
 
     def process_bind_param(self, value, dialect):
         if value is not None:
-            return value[len(self.PREFIX):]
+            return value[len(self.prefix):]
         return value
 
     def process_result_value(self, value, dialect):
         if value is not None:
-            return self.PREFIX + value
+            return self.prefix + value
         return value
